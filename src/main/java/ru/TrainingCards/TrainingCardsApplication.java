85f06e547f35be6842c1c0e5c8d2d0b35b01ad63
AndreiShev <shevelv_andrey@vk.com>
2025-11-15 14:55:03 +0300

    init commit

---
+++ src/main/java/ru/TrainingCards/TrainingCardsApplication.java
@@ -0,0 +1,13 @@
+package ru.TrainingCards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingCardsApplication.class, args);
+	}
+
+}
